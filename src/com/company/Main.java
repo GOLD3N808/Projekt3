4ee21556c9e21a66f43452cc63711eec28c03765
arkmo <ark.mol@wp.pl>
2021-05-22 15:50:20 +0200

    initial commit - zadanie 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Animal dog = new Animal("dog");
+	dog.name = "szarik";
+
+	dog.feed();
+	dog.takeForWalk();
+	dog.feed();
+		dog.takeForWalk();
+		dog.takeForWalk();
+		dog.takeForWalk();
+		dog.takeForWalk();
+		dog.takeForWalk();
+		dog.takeForWalk();
+		dog.takeForWalk();
+		dog.takeForWalk();
+		dog.takeForWalk();
+		dog.takeForWalk();
+		dog.takeForWalk();
+		dog.takeForWalk();
+
+		dog.feed();
+
+
+/*
+	System.out.println(dog.name);
+	System.out.println(dog.species);
+
+	dog.printName();
+	dog.printNameAndOwner("kacper");
+
+	String text = dog.getNameAndOwner("kacper");
+	System.out.println(text);
+
+	Human kacper = new Human();
+	kacper.pet = dog;
+	kacper.firstName = "kacper";
+	kacper.lastName = "Warda";
+
+	System.out.println(kacper.pet.species);
+
+*/
+    }
+}
