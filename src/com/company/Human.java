b152390685662f4c393105fb626b8e5513834639
arkmo <ark.mol@wp.pl>
2021-07-12 14:00:02 +0200

    initial commit - zad. 3 i 4

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,5 +1,5 @@
 package com.company;
-
+import java.util.Date;
 import com.company.Animal;
 
 public class Human  {
@@ -7,5 +7,44 @@ public class Human  {
 
     Animal pet;
     Car car;
+    private Double salary;
+    private String pesel;
+
+
+    public String getPesel() {
+
+        return pesel;
+    }
+
+    public void setPesel(String pesel)
+    {
+
+        this.pesel = pesel;
+    }
+
+
+    public double getSalary() {
+
+        System.out.println(new Date());
+        System.out.println("wypłata " + this.salary);
+        return salary;
+
+    }
+
+    public void setSalary(double salary) {
+        if (salary < 0)
+        {
+            System.out.println("Wartość wypłaty nie może być ujemna");
+        }
+
+        System.out.println("Nowe dane zostały wysłane do systemu");
+
+        System.out.println("Jest koniecznosc odebrania aneksu do umowy od pani Hani z kadr");
+
+        System.out.println("ZUS i US już wiedzą o zmianie wypłaty i nie ma sensu ukrywać dochodu");
+
+        this.salary = salary;
+    }
+
 
 }
